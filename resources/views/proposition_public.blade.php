6944eee7afb474d3fce210d89177e0d8c2d978c1
Photis Avrilionis <photis@avrilionis.com>
2016-11-05 00:55:55 +0100

    Added Google Analytics tracking code on public proposition view

--- resources/views/proposition_public.blade.php
+++ resources/views/proposition_public.blade.php
@@ -19,6 +19,7 @@
 		display: none;
 	}
 </style>
+
 @stop
 
 @section('title', $proposition['propositionSort'])
@@ -170,5 +171,15 @@ function ReplaceNumberWithCommas(yourNumber) {
 	//Combines the two sections
 	return components.join(".");
 }
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-72457439-1', 'auto');
+  ga('send', 'pageview');
+
 </script>
 @stop
